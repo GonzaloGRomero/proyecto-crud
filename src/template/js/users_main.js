26b97bd08eb1fa9f8810c36e096418e6c6ddca83
Your Name <you@example.com>
2022-12-14 13:48:07 -0300

    se corrigio identacion

--- src/template/js/users_main.js
+++ src/template/js/users_main.js
@@ -1,14 +1,14 @@
-if (document.getElementById("racesapp")) {
-    const { createApp } = Vue
- 
-    createApp({ 
+if (document.getElementById("app")) { 
+   const { createApp } = Vue
+
+    createApp({
         data() {
             return {
                 users: [],
                 errored: false,
                 loading: true,
-                url: "http://localhost:5000/users"
-                }
+                url: "http://127.0.0.1:5000/users",
+                };
         },
         methods: {
             fetchData(url) {
@@ -18,25 +18,24 @@ if (document.getElementById("racesapp")) {
                         this.users = data;
                         this.loading = false;
                     })
-                    .catch(err => {
-                        this.errored = true
-                    })
+                    .catch((err) => {
+                        this.errored = true;
+                    });
             },
             eliminar(user) {
                 const url = 'http://localhost:5000/users/' + user;
                 var options = {
                     method: 'DELETE',
-                }
+                };
                 fetch(url, options)
                     .then(res => res.text()) // or res.json()
                     .then(res => {
                         location.reload();
-                    })
-            }
+                    });
+            },
         },
         created() {
-            this.fetchData(this.url)
-        }
-    }).mount('#racesapp')
+            this.fetchData(this.url);
+        },
+    }).mount("#app");
 }
-
