b8da7aac0575e316d4249722871762b914c36cba
Your Name <you@example.com>
2022-12-14 13:43:51 -0300

    se agrego tabla users

--- src/app.py
+++ src/app.py
@@ -3,14 +3,18 @@ from flask import Flask ,jsonify ,request
 from flask_cors import CORS       # del modulo flask_cors importar CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+
 app=Flask(__name__)  # crear el objeto app de la clase Flask
 CORS(app) #modulo cors es para que me permita acceder desde el frontend al backend
+
 # configuro la base de datos, con el nombre el usuario y la clave
 app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:root@localhost/proyecto'
 # desde el objeto app configuro la URI de la BBDD    user:clave@localhost/nombreBBDD
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
 db= SQLAlchemy(app)
 ma=Marshmallow(app)
+
  
 #*********** defino la tabla producto **************
 class Producto(db.Model):   # la clase Producto hereda de db.Model     
@@ -54,12 +58,13 @@ class User(db.Model):   # la clase Producto hereda de db.Model
         
 with app.app_context():
     db.create_all()  # crea las tablas
-
+# with app.app_context():
+#     db.create_all()
 
 #************************************************************
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'nombre', 'lastname', 'race',
+        fields = ('id', 'name', 'lastname', 'race',
                       'gender', 'email', 'password')
 user_schema = UserSchema()            # para crear un user
 users_schema = UserSchema(many=True)  # multiples registros
@@ -183,6 +188,7 @@ def update_user(id):
 
  
 #********************* programa principal *******************************
+
 if __name__=='__main__':  
     app.run(debug=True, port=5000)  
 
